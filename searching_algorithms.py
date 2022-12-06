8aa36123a3db56d19747e9e62dfff97491d39ea6
merkwur <semihiyikalender@gmail.com>
2022-12-06 05:24:31 +0300

    updated

--- searching_algorithms.py
+++ searching_algorithms.py
@@ -9,13 +9,14 @@ import graph
 class GraphSearch:
     
     # initialize the graph here
-    g = graph.Graph()
+    g = graph.Graph(True)
     g.addEdge(0, 1)
     g.addEdge(1, 2)
     g.addEdge(2, 4)
     g.addEdge(3, 0)
     g.addEdge(3, 4)
     g.addEdge(4, 5)
+    print(g.graph)
     g.adjacency_matrix() # for the backtracing algorithm, it is necessary
 
     def __init__(self):
@@ -47,7 +48,7 @@ class GraphSearch:
             return (self.explored, distance)     
         return self.explored
 
-    def back_tracing(self, start_node, end_node):
+    def back_tracing(self, start_node: int, end_node: int) -> int:
         """
         Finds the shortest path between two nodes using an adjacency matrix.
         """
