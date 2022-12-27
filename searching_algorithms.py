96262f1d1af346dab9885e7c84d0bf55da87e417
merkwur <semihiyikalender@gmail.com>
2022-12-27 20:33:58 +0300

    Distance function updated

--- searching_algorithms.py
+++ searching_algorithms.py
@@ -3,6 +3,7 @@
 
 import numpy as np
 import graph 
+import time
 
 class GraphSearch:
     
@@ -10,19 +11,21 @@ class GraphSearch:
     g = graph.Graph(True)
     g.addEdge(0, 1)
     g.addEdge(1, 2)
+    g.addEdge(2, 3)
+    g.addEdge(3, 8)
     g.addEdge(2, 4)
-    g.addEdge(3, 0)
-    g.addEdge(3, 4)
     g.addEdge(4, 5)
-    g.addEdge(4, 6)
+    g.addEdge(5, 6)
     g.addEdge(6, 7)
+
     g.adjacency_matrix() 
 
     def __init__(self):
         self.graph = self.g.graph
         self.adjacency_matrix = self.g.adj_matrix
-        self.explored = []
-        self.d = 0
+        self.BFSExplored = []
+        self.DFSExplored = []
+        self.dist = 0
 
     def BFS(self, start_node: int, end_node: int=0, is_shortest: bool=False) -> tuple[list[int], int] or list[int]: 
         """
@@ -32,45 +35,61 @@ class GraphSearch:
         assert type(end_node) == int, f"Node type({end_node}) must be an integer"
     
         Q = [start_node]
-        self.explored.append(start_node)
+        self.BFSExplored.append(start_node)
 
         while Q:
-            if end_node and end_node in self.explored:
+            if end_node and end_node in self.BFSExplored:
                 break
-
+            
             v = Q.pop(0)
+            
             for w in self.graph[v]:                     
-                if w not in self.explored: 
-                    self.explored.append(w)
+                if w not in self.BFSExplored: 
+                    self.BFSExplored.append(w)
                     Q.append(w) 
-                    
-        if is_shortest:
-            distance = self.distance()
-            return (self.explored, distance)     
-        return self.explored
 
-    def distance(self) -> int:
-        return len(self.explored) - 1
 
-    def DFS(self, start_node: int=0) -> list[int]:
+        if is_shortest:       
+            return (self.BFSExplored, self.distance(end_node))     
+        return self.BFSExplored
+
+    def distance(self, end_node: int) -> int:
+        
+        for i in self.BFSExplored[1:]:
+            self.dist += 1            
+            if len(self.graph[i]) > 1:
+                for j in self.graph[i]:
+                    d = self.DFS(j, True)
+                    self.dist -= len(d) if d[-1] != end_node else 0
+
+        return self.dist
+
+
+    def DFS(self, start_node: int=3, is_distance: bool=False) -> list[int]:
         """
         Depth-first search algorithm (recursive)
         """
         assert type(start_node) == int, f"Node type({start_node}) must be an integer"
+        
+        if is_distance:
+            self.DFSExplored = []
 
-        if start_node not in self.explored:
-            self.explored.append(start_node)
+        if start_node not in self.DFSExplored:
+            self.DFSExplored.append(start_node)
+
+        # print(self.DFSExplored)
 
         for i in self.graph[start_node]:
-            if i not in self.explored:
-                self.explored.append(i)
+            if i not in self.DFSExplored:
+                self.DFSExplored.append(i)
                 self.DFS(i)
         
-        return self.explored
+        return self.DFSExplored
 
 gs = GraphSearch()
 print(gs.graph)
 print("")
 print(gs.adjacency_matrix)
 print("")
-print(gs.BFS(0, 7, True))
+print("BradthFirstSearch", gs.BFS(0, 7, True))
+# print("DepthFirstSearch", gs.DFS(4))
