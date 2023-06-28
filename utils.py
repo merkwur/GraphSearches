d1fdd742ecbd83d22a10fea5bb3697902d6ac916
merkwur <semihiyikalender@gmail.com>
2023-06-28 21:47:21 +0300

    added a visualization

--- utils.py
+++ utils.py
@@ -1,10 +1,11 @@
 # influenced from networkz drawing.
 import numpy.random as npr
 
-def random_layout(G: dict, center: tuple[float or int] = None, dim: int = 2, min_dist: float = 10.) -> dict[int, list]:
+def random_layout(G: dict, center: tuple[float or int] = None, dim: int = 2, min_dist: float = 10.) -> list[list, list]:
     points = [[], []]
     for i in G.keys():
         points[0].append(int(npr.uniform(max(G.keys()) - max(G.keys())/4, max(G.keys()) + max(G.keys())/4, dim)[0] * min_dist))
         points[1].append(int(npr.uniform(max(G.keys()) - max(G.keys())/4, max(G.keys()) + max(G.keys())/4, dim)[1] * min_dist))
     return points
+
 
